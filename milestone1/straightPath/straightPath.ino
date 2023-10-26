1cbb1fa7b435ea1964c0bb220bafe824d672dab5
Ali Nisar <64202928+AliNisar00@users.noreply.github.com>
2023-10-26 10:33:30 +0500

    Upload milestone 1 code
    These are the codes we used when showcasing our model car moving in a straight line and rotating in accordance to milestone 1 requirements.

---
+++ milestone1/straightPath/straightPath.ino
@@ -0,0 +1,40 @@
+// Motor A,B, Arduino and L298N connections
+const int enA = 9;   //ENABLE pin for Motor A to provide PWM 
+const int enB = 10;     //ENABLE pin for Motor B to provide PWM 
+const int in1 = 4;   //IN1 pin for Motor A for direction
+const int in2 = 5;   //IN2 pin for Motor A for direction
+const int in3 = 6;     //IN3 pin for Motor B for direction
+const int in4 = 7;     //IN4 pin for Motor B for direction
+
+void setup() {
+  Serial.begin(115200); //Serial initializing for Debugging
+  
+  //pinModes for enA, in1 and in2 below
+  pinMode(enA, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+
+  //pinModes for enA, in1 and in2 below
+  pinMode(enB, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+
+  
+  // Turn off motors - Initial state
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+
+
+}
+
+void loop() {
+  //Both motors go forward
+    
+  
+    analogWrite(enA, 79); //PWM value for enA
+    analogWrite(enB, 96); //PWM value for enB
+//    delay(3000);
+
+}
